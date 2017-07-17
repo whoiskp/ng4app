dab20e96aca63ed0e27c98230d49a977a9a17213
Whois Kp <kpwhois@gmail.com>
2017-07-17 17:44:16 +0700

    init all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule
